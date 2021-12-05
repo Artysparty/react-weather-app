ea4d64418162960b8edcae9a29ea938b44b2d10b
Artysparty <artyom.kozlov.2000@bk.ru>
2021-12-05 22:38:25 +0300

    add header, thisDay component, styles

---
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './ThisDayInfo.module.scss';
+
+interface Props {
+}
+
+export const Header = (props: Props) => {
+  return <div className={s.header}>
+
+  </div>
+}
---
+++ src/pages/Month-statistics/Month-statistics.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './Month-statistics.module.scss';
+
+interface Props {};
+
+export const MonthStatistics = (props: Props) => {
+  return (
+    <div className={s.monthStatistics}>
+      Month Statistics
+    </div>
+  )
+}
